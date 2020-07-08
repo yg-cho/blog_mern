5eca2ae57aa90216f43cd333ce40fea36f8ebd1f
ygcho <sorisori01@naver.com>
2020-07-08 21:02:21 +0900

    add jwt users routes
    add model/user.js -> compare jtw
    add route/users.js -> tokenGenerator

--- model/user.js
+++ model/user.js
@@ -55,6 +55,14 @@ userSchema.pre("save", async function (next) {
     }
 });
 
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch){
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+
+}
+
 
 
 
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
+const jwt = require("jsonwebtoken");
+
 const userModel = require("../model/user");
+
+
+function tokenGenerator(payload) {
+    return jwt.sign(
+        payload,
+        process.env.SECRET_KEY,
+        {expiresIn: 36000 }
+    );
+}
+
+
+
+
 
 
 // @route   POST http://localhost:5000/users/register
@@ -49,12 +64,50 @@ router.post("/register", (req, res) => {
 // @desc    Logged User
 // @access  Public
 router.post("/login", (req, res) => {
-
+    const {email, password} = req.body;
+    userModel
+        .findOne({ email })
+        .then(user => {
+            if(!user) {
+                return res.json({
+                    message: "email not found"
+                });
+            } else {
+                user.comparePassword(password, (err, isMatch) => {
+                    if(err || isMatch === false) {
+                        return res.json({
+                            message: "password incorrect"
+                        });
+                    } else {
+                        // token return
+                        const payload = {
+                            id: user._id,
+                            name: user.name,
+                            avatar: user.avatar
+                        };
+                        res.json({
+                            success: isMatch,
+                            token: "Bearer " + tokenGenerator(payload)
+                        });
+                    }
+                })
+            }
+        })
+        .catch(err => {
+            res.json({
+                error: err.message
+            });
+        });
 });
 
 
 
+// @route  GET /users/current
+// @desc   Return current user
+// @access private
+router.get('/current', (req, res) => {
 
+});
 
 
 
