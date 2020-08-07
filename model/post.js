a098118fe53ea4cd3858d456d715a893a8084f27
ygcho <sorisori01@naver.com>
2020-08-07 09:59:24 +0900

    add post model reply

--- model/post.js
+++ model/post.js
@@ -25,7 +25,28 @@ const postSchema = new mongoose.Schema(
                 }
             }
         ],
-        reply: [],
+        reply: [
+            {
+                user: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: 'user'
+                },
+                text: {
+                    type: String,
+                    required: true
+                },
+                name: {
+                    type: String,
+                },
+                avatar: {
+                    type: String
+                },
+                date: {
+                    type: Date,
+                    default: Date.now
+                }
+            }
+        ],
 
     },
     {
