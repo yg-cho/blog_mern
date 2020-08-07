4676ff30f79f851e2c0014841299313080937ce5
ygcho <sorisori01@naver.com>
2020-08-07 11:24:15 +0900

    refactoring user register ( send email )

--- controller/user.js
+++ controller/user.js
@@ -1,8 +1,10 @@
 const jwt = require("jsonwebtoken");
+const sgMail = require("@sendgrid/mail");
 const userModel = require("../model/user");
 const validateRegisterInput = require("../validation/register");
 const validateLoginInput = require("../validation/login");
 
+sgMail.setApiKey(process.env.MAIL_KEY)
 
 
 function tokenGenerator(payload) {
@@ -34,20 +36,52 @@ exports.register_user = (req, res) => {
                 //    message : "email already used"
                 // });
             } else {
-                const newUser = new userModel({
-                    name,
-                    email,
-                    password
-                });
-                newUser
-                    .save()
-                    .then(user => {
-                        res.json({
-                            msg: "Successful newUser",
-                            userInfo : user,
-                        });
+                const token = jwt.sign(
+                    { name, email, password },
+                    process.env.JWT_ACCOUNT_ACTIIVATION,
+                    { expiresIn : '10m'}
+                )
+                console.log(token);
+
+                const emailData = {
+                    from: process.env.EMAIL_FROM,
+                    to: email,
+                    subject: 'Account activation link',
+                    html: `
+                    <h1>Please use the following to activate your account</h1>
+                    <p>${process.env.CLIENT_URL}/users/activate/${token}</p>
+                    <hr />
+                    <p>This email may containe sensetive information</p>
+                    <p>${process.env.CLIENT_URL}</p>
+                    `
+                };
+                sgMail
+                    .send(emailData)
+                    .then(sent => {
+                        return res.status(200).json({
+                            message: `Email has been sent to ${email}`
+                        })
                     })
-                    .catch(err => res.json(err));
+                    .catch(err => {
+                        return res.status(400).json({
+                            success: false,
+                            error: err
+                        });
+                    });
+                // const newUser = new userModel({
+                //     name,
+                //     email,
+                //     password
+                // });
+                // newUser
+                //     .save()
+                //     .then(user => {
+                //         res.json({
+                //             msg: "Successful newUser",
+                //             userInfo : user,
+                //         });
+                //     })
+                //     .catch(err => res.json(err));
             }
 
         })
